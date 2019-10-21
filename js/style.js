36414af22e4408627563cdd21bb1e29acaf5e6c8
antonioseguil <antonioseguil@gmail.com>
2019-10-21 00:49:09 -0500

    version 1.0.0 diseño base

---
+++ js/style.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    navBar();
+    addCalendar();
+    addCalendarTerapista();
+});
+
+//agregado de clase cuando se da click
+function navBar() {
+    $('.nav-link').click(function () {
+        removeActive();
+        $(this).addClass('active');
+    });
+}
+
+//quitando la clase active
+function removeActive() {
+    $('.active').removeClass('active');
+};
+// full calendar
+
+function addCalendar() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        locale: 'es',
+        plugins: ['dayGrid'],
+        height: 300
+        //contentHeight: 100
+    });
+    calendar.render();
+}
+
+
